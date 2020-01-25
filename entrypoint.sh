dd62ff54570428fb7ccd00820b4f1b17faed40ef
Daniel Connolly <d.connolly@nchain.com>
2020-01-25 15:58:00 +0000

    updates for 1.14.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,6 @@
 
 if [[ "$1" == "electrumx_server" ]]; then
 	CMD="$1"
-	shift
-	if [[ -s "/run/secrets/bchrpcuser" ]]; then
-		BCH_RPC_USER="$(cat /run/secrets/bchrpcuser)"
-	fi
-	if [[ -s "/run/secrets/bchrpcpassword" ]]; then
-		BCH_RPC_PASSWORD="$(cat /run/secrets/bchrpcpassword)"
-	fi
-	export DAEMON_URL="$BCH_RPC_USER:$BCH_RPC_PASSWORD@$DAEMON_HOST:$DAEMON_PORT"
 	exec "$CMD" 
 fi
 
